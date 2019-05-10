5fe40b810ab82e244668f859788160361e0e275e
Martin <msmith3dart@icloud.com>
2019-05-10 09:13:34 -0700

    fix and comments add to file
    fix namespace for import of  BaseModel
    added comments next to attribute which are references to the key in the Json the API returns.

--- models/recipe.py
+++ models/recipe.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 """This File defines the Recipe model"""
-import models
-from models.basemodel import BaseModel
+from models.base_model import BaseModel
 
 
 class Recipe(BaseModel):
     """Recipe class that declares the attribute involving a recipe"""
-    title = ""          # api: title
-    image_url = ""      # api: image
-    source_url = ""     # api: sourceUrl
-    cook_in_min = 0     # api: cookingMinutes
-    prep_in_min = 0     # api: preparationMinutes
-    dish_type = []      # api: dishTypes
-    ingredients = []    # api: ingredients
-    servings = 0        # api: servings
-    description = ""    # api: description
+    title = ""  # api: title
+    image_url = ""  # api: image
+    source_url = ""  # api: sourceUrl
+    cook_in_min = 0  # api: cookingMinutes
+    prep_in_min = 0  # api: preparationMinutes
+    dish_type = []  # api: dishTypes
+    ingredients = []  # api: ingredients
+    servings = 0  # api: servings
+    description = ""  # api: description
 
-    api_id = 0          # api: id
-    api_url = ""        # api: spoonacularSourceUrl
+    api_id = 0  # api: id
+    api_url = ""  # api: spoonacularSourceUrl
+
+    # TODO: Add property setters and getters
+
