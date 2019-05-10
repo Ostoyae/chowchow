91479b400e1b90b5f12caff0cc41ba702f4f5b22
Martin <msmith3dart@icloud.com>
2019-05-10 15:27:24 -0700

    added method to added a recipe
    like() dislike() and delete _from_fav()

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import json
 import unittest
 import models
+from models.recipe import Recipe
 from os import environ
 from datetime import datetime
 
@@ -8,7 +9,7 @@ from datetime import datetime
 from App.fetcher import Fetcher
 
 
-class test_app(unittest.TestCase):
+class test_app_backend(unittest.TestCase):
 
     def test_dotenv_rapidAPI_host(self):
         self.assertIsNotNone(environ.get("X-RAPIDAPI-HOST", None))
@@ -25,20 +26,55 @@ class test_app(unittest.TestCase):
         with open('test_data.json', 'w+') as j:
             json.dump(fetch.json, j)
 
-    def test_create_recipe(self):
+    @unittest.skip("not test this now")
+    def test_create_recipe_cache(self):
         fetch = Fetcher()
-        with open('test_data.json', 'r') as f:
+        with open('test_data_0.json', 'r') as f:
             j = json.load(f)
             print(j)
             fetch.create_recipe(j)
             models.storage.reload("objects")
             print(models.storage.all())
 
-    def test_create_recipe(self):
-        fetch = Fetch()
-        with open('test_data.json', 'r') as f:
+    @unittest.skip("not test this now")
+    def test_create_recipe_fav(self):
+        fetch = Fetcher()
+        with open('test_data_0.json', 'r') as f:
             j = json.load(f)
             print(j)
             fetch.create_recipe(j)
-            models.storage.save_to_favorites() 
+            models.storage.save("favorites")
             models.storage.reload("favorites")
+
+    def test_like_recipe(self):
+        new_r = dict(
+            id="abcd",
+            title="a new recipe"
+        )
+        r = Recipe(**new_r)
+        r.save()
+        r.like()
+        self.assertIsNotNone(models.storage.get("Recipe", "abcd", "fav"))
+    def test_Dislike_recipe(self):
+        new_r = dict(
+            id="xyz",
+            title="I don't like you!"
+        )
+        r = Recipe(**new_r)
+        r.save()
+        r.dislike()
+        self.assertIsNone(models.storage.get("Recipe", "xzy"))
+        self.assertIsNone(models.storage.get("Recipe", "xzy", "fav"))
+
+    def test_delete_from_like_recipe(self):
+        new_r = dict(
+            id="acbeasyas123",
+            title="a new recipe"
+        )
+        r = Recipe(**new_r)
+        r.save()
+        r.like()
+        self.assertIsNotNone(models.storage.get("Recipe", "acbeasyas123", "fav"))
+        r.delete_from_fav()
+        self.assertIsNone(models.storage.get("Recipe", "acbeasyas123", "fav"))
+
